5b8a17bee5417fd754f166b02604ee54831a8f72
andrielb93 <andriel.bueno93@gmail.com>
2024-04-15 16:26:25 -0300

    first commit

---
+++ aula4.rb
@@ -0,0 +1,15 @@
+#1)saída de dados na tela
+#usuario digite seu nome
+print "digite seu nome: "
+
+#2)programa permita que o usuario digite o que foi pedido
+#gets.chomp
+nome = gets.chomp
+
+print "digite seu sobrenome: "
+sobrenome = gets.chomp
+
+
+#3)saída final
+#para chamar um dado #{variavel}
+puts "oi #{nome} #{sobrenome}!"
---
+++ aula4soma.rb
@@ -0,0 +1,12 @@
+#usuario digite um numero
+#pedir ao ususario
+#.to_i
+print "Digite um numero: "
+num1 = gets.chomp.to_i
+#usuario digite outro numero
+print "Digite outro numero: "
+num2 = gets.chomp.to_i
+#some esses numeros do ususario
+soma = num1 + num2
+#apareça na tela
+puts "O resultado da soma é #{soma}"
